5d0e2d1f2d36b241a4cc68e21bf865d336901e91
DannyPratama088 <dannypratama088@gmail.com>
2022-10-07 14:56:53 +0700

    update

---
+++ pustaka-booking/application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Danny Pratama Putra
+ Saya tingga di daerah Pangeran Jayakarta
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
